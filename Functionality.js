1c896e683b24db5dddf10a2c068c83d87b4779da
Tyler Scheifley <Tscheifley7628@conestogac.on.ca>
2021-10-28 16:00:18 -0400

    added new task box and menu

--- Functionality.js
+++ Functionality.js
@@ -1,4 +1,18 @@
 const todos = document.querySelectorAll(".todo");
+const toggle =document.getElementById("toggle")
+const exit =document.getElementById("close")
+const submit =document.getElementById("modal")
+const add =document.getElementById("open")
+
+//toggle navigation 
+toggle.addEventListener('click', () => document.body.classList.toggle("show-nav"));
+
+//show modal 
+
+add.addEventListener('click',() => modal.classList.add("show-modal")) 
+
+exit.addEventListener('click',() => modal.classList.remove("show-modal"))
+
 
 todos.forEach(todo =>{
     todo.addEventListener("click", () =>{
